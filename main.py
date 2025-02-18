f726c6a90931aead2df0ce333a2f779a21362090
Obeida Iqbal <oiqbal@umich.edu>
2025-02-18 17:30:41 -0500

    added function that allows you to save comments to a json so you can manually edit sentiment for training the NLP model

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 
 from transformers import pipeline, AutoConfig
 import praw
+import json
 
 sentiment_model = pipeline("sentiment-analysis", model = "akshataupadhye/finetuning-sentiment-model-reddit-data")
 
@@ -65,15 +66,45 @@ def print_results(results, comment_count):
     for sentiment in results:
         print(f"{sentiment}: {(results[sentiment] / comment_count) * 100}%")
 
+def get_training_data(count):
+    """Gets the top posts from the subreddit to be used for training"""
+    reddit = praw.Reddit(
+        client_id = read_file("clientid.txt"), 
+        client_secret = read_file("secret.txt"), 
+        user_agent = "DiscussionSentimentAnalysis/0.1 by u/chikachika-boomboom" 
+    )
+    subreddit = reddit.subreddit("OnePiece")
+    hot_posts = subreddit.hot(limit = count)
+    return hot_posts
+
+def save_comments(post, filename = "comments.json"):
+    """Saves comments from post to comments.json for training"""
+    post.comment_sort = "top"
+    post.comments.replace_more(limit = 0)
+    data = []
+    for comment in post.comments.list():
+        data.append({"text": comment.body, "label": None})
+    with open(filename, "w", encoding = "utf-8") as f:
+        json.dump(data, f, indent = 4)
+    print(f"Saved {len(data)} comments to `{filename}` for training.")
 
 def main():
     """Main function"""
-    current = get_post()
-    comment_count = 200
-    comments = get_comments(current, comment_count)
-    results = {"positive": 0, "neutral": 0, "negative": 0}
-    analyze_comments(comments, results)
-    print_results(results, comment_count)
+    instruction = input("Would you like to analyze or train the model(a/t)?: ")
+    if instruction == "a":
+        current = get_post()
+        comment_count = 200
+        comments = get_comments(current, comment_count)
+        results = {"positive": 0, "neutral": 0, "negative": 0}
+        analyze_comments(comments, results)
+        print_results(results, comment_count)
+    elif instruction == "t":
+        count = int(input("How many posts would you like to use: "))
+        hot_posts = get_training_data(count)
+        for post in hot_posts:
+            save_comments(post)
+    else:
+        print("Unkown input")
 
 if __name__ == "__main__":
     main()
