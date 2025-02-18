d42b7ec9a5322d2ed255fb16469e81c31c506bcb
Obeida Iqbal <oiqbal@umich.edu>
2025-02-18 12:10:42 -0500

    installed praw into venv and connected account

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-# This program prints Hello, world!
+# Reddit Sentiment Analysis Bot
+# This script is intended to fetch comments from a subreddit post 
+# using PRAW and prepares them for sentiment analysis using an LLM.
+# Author: Obeida Iqbal
 
+import praw
+
+
+def read_file(filename):
+    file = open(filename, "r")
+    return file.read()
+
+reddit = praw.Reddit(
+    client_id = read_file("clientid.txt"), 
+    client_secret = read_file("secret.txt"), 
+    user_agent = "DiscussionSentimentAnalysis/0.1 by chikachika-boomboom" 
+)
+print(reddit.read_only)
 print("Hello, world!")
